cc63d6d63fe7b104b5d85fefef9b344762090a04
Nguyen Hoang <hoang.pqn2123@gmail.com>
2024-11-10 21:08:03 +0700

    bash script

---
+++ bash_sh/train_resunet.sh
@@ -0,0 +1,8 @@
+python -m neopolyp.train \
+        --model resunet \
+        --batch_size 16 \
+        --max_epochs 50 \
+        --num_workers 4 \
+        --lr 0.0001 \
+        --split_ratio 0.96 \
+        --data_path /Users/pqnhhh/Desktop/BKAI IGH NeoPolyp
